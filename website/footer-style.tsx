6653432610b898f5c9f9fd34aa463df31dc0beb0
MichaelSofa <lxxc521@163.com>
2025-04-25 17:39:42 +0800

    增加工信部超链接

--- website/footer-style.tsx
+++ website/footer-style.tsx
@@ -29,7 +29,7 @@ export const FooterContent = () => (
           <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
           </svg>
-          ICP备案：渝ICP备2025052425号-1
+          ICP备案：渝ICP备2025052425号
         </a>
         <a href="https://beian.mps.gov.cn/#/query/webSearch?code=50010302505359" target="_blank" rel="noopener noreferrer" className="flex items-center hover:text-blue-500 transition-colors">
           <img src="/static/china-police.png" alt="公安备案图标" className="h-4 mr-1" />
